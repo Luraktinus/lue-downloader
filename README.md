77a0bf187dd1af107ed5e3716fa2d9af443ff6c4
Lue <lue@acer-deb3.local>
2018-02-02 17:37:05 +0100

    being polite and changing the readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # lue-downloader
 a simple youtube-dl frontent written in Godot 3
+
+still a prototype and very hacky
--- core.gd
+++ core.gd
@@ -6,10 +6,10 @@ func _on_Button_pressed():
 	zero = ""
 	link = get_node("LineEdit").text
 	if link == zero:
-		OS.alert("You have to enter a link, dumbass")
-		return
-	OS.alert("Downloading " + link, "CR-D")
-	OS.execute("gnome-terminal", ["--", "youtube-dl", link],true)
+		OS.alert("You have to enter a link!")
+	else:
+		OS.alert("Downloading " + link, "Lue-D")
+		OS.execute("gnome-terminal", ["--", "youtube-dl", link],true)
 func _on_Help_pressed():
 	OS.alert(
 	#Readme, dont forget the "+"\n"+"
@@ -19,7 +19,6 @@ func _on_Help_pressed():
 	"...might change in future versions "+"\n"+
 	"also you need the cookies.txt for CH in your home dir "+"\n"+
 	""
-	
 	#Title
 	,"Help")
 	
